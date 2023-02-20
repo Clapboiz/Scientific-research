8e4ae9a673d8e7e9c8faf5ee896e329efba317b4
Clapboiz <phamconglap03@gmail.com>
2023-02-21 03:11:48 +0700

    First

---
+++ Marchine learning/Code_py/soft_margin_svm.py
@@ -0,0 +1,34 @@
+# To support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+# list of points
+import numpy as np
+import matplotlib.pyplot as plt
+#thu vien dung de tinh toan khoang cach trong matrix
+from scipy.spatial.distance import cdist
+from matplotlib.backends.backend_pdf import PdfPages
+np.random.seed(22)
+
+means = [[2, 2], [4, 2]]
+cov = [[.7, 0], [0, .7]]
+N = 20
+# dung de ve ngau nhien  tu cac mau da bien
+X0 = np.random.multivariate_normal(means[0], cov, N) # each row is a data point
+X1 = np.random.multivariate_normal(means[1], cov, N)
+
+with PdfPages('data.pdf') as pdf:
+    plt.plot(X0[:, 0], X0[:, 1], 'bs', markersize = 8, alpha = 1)
+    plt.plot(X1[:, 0], X1[:, 1], 'ro', markersize = 8, alpha = 1)
+    plt.axis('equal')
+    plt.ylim(0, 4)
+    plt.xlim(0, 5)
+
+    # hide tikcs
+    cur_axes = plt.gca()
+    cur_axes.axes.get_xaxis().set_ticks([])
+    cur_axes.axes.get_yaxis().set_ticks([])
+
+    plt.xlabel('$x_1$', fontsize = 20)
+    plt.ylabel('$x_2$', fontsize = 20)
+    pdf.savefig()
+    # plt.savefig('logistic_2d.png', bbox_inches='tight', dpi = 300)
+    plt.show()
