37cfdd59b61fc093a7df57e94fbc03c657093a3a
Clapboiz <phamconglap03@gmail.com>
2023-03-06 20:33:36 +0700

    Update

--- Marchine learning/Code_py/pr_spam_emails/pr_spam_emails.py
+++ Marchine learning/Code_py/pr_spam_emails/pr_spam_emails.py
@@ -8,18 +8,17 @@ from sklearn.naive_bayes import MultinomialNB, BernoulliNB, GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import cross_val_score
-
+from sklearn.metrics import precision_score, recall_score, classification_report, confusion_matrix
 
 #import model classification
 from sklearn import svm
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC, LinearSVC
 from sklearn.neighbors import KNeighborsClassifier
 
-
+from scipy.sparse import csr_matrix
 # Đọc dữ liệu email từ tập tin
 emails = pd.read_csv("spam_emails_dataset.csv")
 print(emails.head())
@@ -54,39 +53,47 @@ print(y_pred)
 #train hang loat model theo kieu cross , nghia la theo tung fold
 seed = 2023
 models = [
-    LinearSVC(random_state=seed),
+    LinearSVC(max_iter=12000,random_state=seed),
     SVC(random_state=seed),
     KNeighborsClassifier(metric='minkowski', p=2),
     DecisionTreeClassifier(random_state=seed),
     RandomForestClassifier(random_state=seed),
+    MultinomialNB(),
+    BernoulliNB(),
+    # bug GaussianNB() em fix chua duoc :"))
+    # GaussianNB() #typeError: A sparse matrix was passed, but dense data is required. Use X.toarray() to convert to a dense numpy array.
 ]
 
 from sklearn.model_selection import StratifiedKFold
+def generate_baseline_results(models, X, labels, metrics, cv=5, plot_results=False):
 
-#create func train other mopdels
-def generate_baseline_results(models, X, labels, metrics, cv=5, plot_results = False):
-    #define k-fold
+    # define k-fold:
     kfold = StratifiedKFold(cv, shuffle=True, random_state=seed)
     entries = []
     for model in models:
         model_name = model.__class__.__name__
-        scores = cross_val_score(model, X, labels, scoring= metrics, cv=kfold)
+        print(model_name)
+        scores = cross_val_score(model, X, labels, scoring=metrics, cv=kfold)
         for fold_idx, score in enumerate(scores):
             entries.append((model_name, fold_idx, score))
+
     cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_id', 'accuracy_score'])
-
+    print(cv_df)
     if plot_results:
-        sns.boxplot(X = 'model_name', labels='accuracy_score', data = cv_df, color = 'lightblue', showmeans = True)
-        plt.title("using 5 kfold cross val")
+        sns.boxplot(x='model_name', labels='accuracy_score', data=cv_df, color='lightblue', showmeans=True)
+        plt.title("Boxplot of Base-Line Model Accuracy using 5-fold cross-validation")
+        plt.xticks(rotation=45)
         plt.show()
-    #summary results
-    mean = cv_df.groupby('model_name')['accuracy_score'].mean()
 
-    baseline_results = pd.concat(mean, axis= 1, ignore_index= True)
+    # Summary result
+    mean = cv_df.groupby('model_name')['accuracy_score'].mean()
+    std = cv_df.groupby('model_name')['accuracy_score'].std()
+
+    baseline_results = pd.concat([mean, std], axis=1, ignore_index=True)
     baseline_results.columns = ['Mean', 'Standard Deviation']
 
-    #sort by accuracy
-    baseline_results.sort_values(by = ['Mean'], ascending = False, implace = True)
-    return baseline_results
+    # sort by accuracy
+    baseline_results.sort_values(by=['Mean'], ascending=False, inplace=True)
+    print(baseline_results)
 
-generate_baseline_results(models, X, labels, metrics="accuracy", cv = 5, plot_results= False)
+generate_baseline_results(models, X, labels, metrics='accuracy', cv=5, plot_results=False)
