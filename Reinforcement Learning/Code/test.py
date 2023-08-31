c82dc29716c0476e779de3d87706c440709172c4
Clapboiz <phamconglap03@gmail.com>
2023-09-01 02:40:29 +0700

    Update

--- Reinforcement Learning/Code/test.py
+++ Reinforcement Learning/Code/test.py
@@ -1,103 +1,304 @@
+# Deep Q Learning / Keras / OpenAI / Frozen Lake / Not Slippery / 5x5
 import gym
-import numpy
+import numpy as np
 import random
-from os import system, name
-from time import sleep
+import matplotlib.pyplot as plt
+import matplotlib
+import os
 
-# Define function to clear console window.
-def clear(): 
-  
-    # Clear on Windows.
-    if name == 'nt': 
-        _ = system('cls')
-  
-    # Clear on Mac and Linux. (os.name is 'posix') 
-    else: 
-        _ = system('clear')
+from keras.optimizers import Adam
+from keras.layers import Dense
+from keras.models import Sequential
+from collections import deque
+from tensorflow.keras.layers import Dropout
 
-clear()
+env = gym.make("FrozenLake-v1")
+train_episodes=400
+test_episodes=100
+max_steps=750
+state_size = env.observation_space.n
+action_size = env.action_space.n
+batch_size=3000
 
-"""Setup"""
+class Agent:
+    def __init__(self, state_size, action_size):
+        self.memory = deque(maxlen=2500)
+        self.learning_rate=0.8
+        self.epsilon=1
+        self.max_eps=1
+        self.min_eps=0.01
+        self.eps_decay = 0.001/3
+        self.gamma=0.9
+        self.state_size= state_size
+        self.action_size= action_size
+        self.epsilon_lst=[]
+        self.model = self.buildmodel()
 
-env = gym.make("Taxi-v3").env # Setup the Gym Environment
+    def buildmodel(self):
+        model=Sequential()
+        model.add(Dense(32, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(32, activation='relu'))
+        model.add(Dropout(0.3))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
 
-# Make a new matrix filled with zeros.
-# The matrix will be 500x6 as there are 500 states and 6 actions.
-q_table = numpy.zeros([env.observation_space.n, env.action_space.n])
+    def add_memory(self, new_state, reward, done, state, action):
+        self.memory.append((new_state, reward, done, state, action))
 
-training_episodes = 20000 # Amount of times to run environment while training.
-display_episodes = 10 # Amount of times to run environment after training.
+    def action(self, state):
+        if np.random.rand() > self.epsilon:
+            return np.random.randint(0,4)
+        return np.argmax(self.model.predict(state))
 
-# Hyperparameters
-alpha = 0.1 # Learning Rate
-gamma = 0.6 # Discount Rate
-epsilon = 0.1 # Chance of selecting a random action instead of maximising reward.
+    def pred(self, state):
+        return np.argmax(self.model.predict(state))
 
-# For plotting metrics
-all_epochs = []
-all_penalties = []
+    def replay(self,batch_size):
+        minibatch=random.sample(self.memory, batch_size)
+        for new_state, reward, done, state, action in minibatch:
+            target= reward
+            if not done:
+                target=reward + self.gamma* np.amax(self.model.predict(new_state))
+            target_f= self.model.predict(state)
+            target_f[0][action]= target
+            self.model.fit(state, target_f, epochs=3, verbose=0)
 
-"""Training the Agent"""
+        if self.epsilon > self.min_eps:
+            self.epsilon=(self.max_eps - self.min_eps) * np.exp(-self.eps_decay*episode) + self.min_eps
 
-for i in range(training_episodes):
-    state = env.reset()[0] # Reset returns observation state and other info. We only need the state.
+        self.epsilon_lst.append(self.epsilon)
+
+    def load(self, name):
+        self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
+
+agent=Agent(state_size, action_size)
+
+reward_lst=[]
+for episode in range(train_episodes):
+    state= env.reset()
+    state_arr=np.zeros(state_size)
+    state_arr[int(state[0])] = 1
+    state= np.reshape(state_arr, [1, state_size])
+    reward = 0
     done = False
-    penalties, reward, = 0, 0
-    
-    while not done:
-        if random.uniform(0, 1) < epsilon:
-            action = env.action_space.sample() # Pick a new action for this state.
-        else:
-            action = numpy.argmax(q_table[state]) # Pick the action which has previously given the highest reward.
+    for t in range(max_steps):
+        # env.render()
+        action = agent.action(state)
+        result = env.step(action)
+        new_state = result[0]
+        reward += result[1]  # Cộng dồn phần thưởng
+        done = result[2]
+        new_state_arr = np.zeros(state_size)
+        new_state_arr[new_state] = 1
+        new_state = np.reshape(new_state_arr, [1, state_size])
+        agent.add_memory(new_state, reward, done, state, action)
+        state= new_state
 
-        next_state, reward, done, info = env.step(action) 
-        
-        old_value = q_table[state, action] # Retrieve old value from the q-table.
-        next_max = numpy.max(q_table[next_state])
+        if done:
+            print(f'Episode: {episode:4}/{train_episodes} and step: {t:4}. Eps: {float(agent.epsilon):.2}, reward {reward}')
+            break
 
-        # Update q-value for current state.
-        new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
-        q_table[state, action] = new_value
+    reward_lst.append(reward)
 
-        if reward == -10: # Checks if agent attempted to do an illegal action.
-            penalties += 1
+    if len(agent.memory)> batch_size:
+        agent.replay(batch_size)
 
-        state = next_state
-        
-    if i % 100 == 0: # Output number of completed episodes every 100 episodes.
-        print(f"Episode: {i}")
+print(' Train mean % score= ', round(100*np.mean(reward_lst),1))
 
-print("Training finished.\n")
+mean_reward = np.mean(reward_lst)
+std_reward = np.std(reward_lst)
+print(f"Mean_reward={mean_reward:.2f} +/- {std_reward:.2f}")
 
-"""Display and evaluate agent's performance after Q-learning."""
-
-total_epochs, total_penalties = 0, 0
-
-for _ in range(display_episodes):
+# test
+test_wins=[]
+for episode in range(test_episodes):
     state = env.reset()
-    epochs, penalties, reward = 0, 0, 0
-    
+    state_arr=np.zeros(state_size)
+    state_arr[int(state[0])] = 1
+    state= np.reshape(state_arr, [1, state_size])
     done = False
-    
-    while not done:
-        action = numpy.argmax(q_table[state])
-        state, reward, done, info = env.step(action)
+    reward=0
+    state_lst = []
+    state_lst.append(state)
+    print('******* EPISODE ',episode, ' *******')
 
-        if reward == -10:
-            penalties += 1
+    for step in range(max_steps):
+        action = agent.pred(state)
+        result = env.step(action)
+        new_state = result[0]
+        reward += result[1]  # Cộng dồn phần thưởng
+        done = result[2]
+        new_state_arr = np.zeros(state_size)
+        new_state_arr[new_state] = 1
+        new_state = np.reshape(new_state_arr, [1, state_size])
+        state = new_state
+        state_lst.append(state)
+        if done:
+            print(reward)
+            # env.render()
+            break
 
-        epochs += 1
-        clear()
-        env.render()
-        print(f"Timestep: {epochs}")
-        print(f"State: {state}")
-        print(f"Action: {action}")
-        print(f"Reward: {reward}")
-        sleep(0.15) # Sleep so the user can see the 
+    test_wins.append(reward)
+env.close()
 
-    total_penalties += penalties
-    total_epochs += epochs
+print(' Test mean % score= ', int(100*np.mean(test_wins)))
 
-print(f"Results after {display_episodes} episodes:")
-print(f"Average timesteps per episode: {total_epochs / display_episodes}")
-print(f"Average penalties per episode: {total_penalties / display_episodes}")
+fig=plt.figure(figsize=(10,12))
+matplotlib.rcParams.clear()
+matplotlib.rcParams.update({'font.size': 16})
+plt.subplot(311)
+plt.scatter(list(range(len(reward_lst))), reward_lst)
+plt.title('5x5 Frozen Lake Result(DQN) \n \nTrain Score')
+plt.ylabel('Score')
+plt.xlabel('Episode')
+
+plt.subplot(312)
+plt.scatter(list(range(len(agent.epsilon_lst))), agent.epsilon_lst)
+plt.title('Epsilon')
+plt.ylabel('Epsilon')
+plt.xlabel('Episode')
+
+plt.subplot(313)
+plt.scatter(list(range(len(test_wins))), test_wins)
+plt.title('Test Score')
+plt.ylabel('Score')
+plt.xlabel('Episode')
+plt.ylim((0,1.1))
+plt.savefig('5x5resultdqn.png',dpi=300)
+plt.show()
+
+# import tensorflow as tf
+# import numpy as np
+# import gym
+# import matplotlib.pyplot as plt
+
+# #
+# # from gym.envs.registration import register
+# # # register(
+# # #     id='FrozenLakeNotSlippery-v0',
+# # #     entry_point='gym.envs.toy_text:FrozenLakeEnv',
+# # #     kwargs={'map_name' : '4x4', 'is_slippery': False},
+# # #     max_episode_steps=100,
+# # #     reward_threshold=0.78, # optimum = .8196
+# # # )
+
+
+# tf.set_random_seed(1)
+# np.random.seed(1)
+
+# class DQN():
+#     def __init__(self,nstate,naction):
+#         self.nstate=nstate
+#         self.naction=naction
+#         self.sess = tf.Session()
+#         self.memcnt=0
+#         self.BATCH_SIZE = 64
+#         self.LR = 0.0012                      # learning rate
+#         self.EPSILON = 0.92                 # greedy policy
+#         self.GAMMA = 0.9999                   # reward discount
+#         self.MEM_CAP = 2000
+#         self.mem= np.zeros((self.MEM_CAP, self.nstate * 2 + 2))     # initialize memory
+#         self.updataT=150
+#         self.built_net()
+
+
+#     def built_net(self):
+#         self.s = tf.placeholder(tf.float64, [None,self.nstate])
+#         self.a = tf.placeholder(tf.int32, [None,])
+#         self.r = tf.placeholder(tf.float64, [None,])
+#         self.s_ = tf.placeholder(tf.float64, [None,self.nstate])
+
+#         with tf.variable_scope('q'):                                  # evaluation network
+#             l_eval = tf.layers.dense(self.s, 10, tf.nn.relu, kernel_initializer=tf.random_normal_initializer(0, 0.1))
+#             self.q = tf.layers.dense(l_eval, self.naction, kernel_initializer=tf.random_normal_initializer(0, 0.1))
+
+#         with tf.variable_scope('q_next'):                                           # target network, not to train
+#             l_target = tf.layers.dense(self.s_, 10, tf.nn.relu, trainable=False)
+#             q_next = tf.layers.dense(l_target, self.naction, trainable=False)
+
+#         q_target = self.r + self.GAMMA * tf.reduce_max(q_next, axis=1)    #q_next:  shape=(None, naction),
+#         a_index=tf.stack([tf.range(self.BATCH_SIZE,dtype=tf.int32),self.a],axis=1)
+#         q_eval=tf.gather_nd(params=self.q,indices=a_index)
+#         loss=tf.losses.mean_squared_error(q_target,q_eval)
+#         self.train=tf.train.AdamOptimizer(self.LR).minimize(loss)
+#         #  q现实target_net- Q估计
+#         self.sess.run(tf.global_variables_initializer())
+
+#     def choose_action(self,status):
+#         fs = np.zeros((1,self.nstate))
+#         fs[0,status]=1.0  # ONE HOT
+#         if  np.random.uniform(0.0,1.0)<self.EPSILON:
+#             action=np.argmax( self.sess.run(self.q,feed_dict={self.s:fs}))
+#         else:
+#             action=np.random.randint(0,self.naction)
+#         return action
+
+#     def learn(self):
+#         if(self.memcnt%self.updataT==0):
+#             t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='q_next')
+#             e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='q')
+#             self.sess.run([tf.assign(t, e) for t, e in zip(t_params, e_params)])
+#         rand_indexs=np.random.choice(self.MEM_CAP,self.BATCH_SIZE,replace=False)
+#         temp=self.mem[rand_indexs]
+#         bs = temp[:,0:self.nstate]#.reshape(self.BATCH_SIZE,NSTATUS)
+#         ba = temp[:,self.nstate]
+#         br = temp[:,self.nstate+1]
+#         bs_ = temp[:,self.nstate+2:]#.reshape(self.BATCH_SIZE,NSTATUS)
+#         self.sess.run(self.train, feed_dict={self.s:bs,self.a:ba,self.r:br,self.s_:bs_})
+
+
+#     def storeExp(self,s,a,r,s_):
+#         fs = np.zeros(self.nstate)
+#         fs[s] = 1.0                       # ONE HOT
+#         fs_ = np.zeros(self.nstate)
+#         fs_[s_] = 1.0                          # ONE HOT
+#         self.mem[self.memcnt%self.MEM_CAP]=np.hstack([fs,a,r,fs_])
+#         self.memcnt+=1
+
+
+#     def run(self,numsteps):
+#         cnt_win =0
+#         all_r=0.0
+#         win_rate=[]
+#         for i in range(numsteps):
+#             s=env.reset()
+#             done=False
+#             while(not done):
+#                 a=self.choose_action(s)
+#                 s_,r,done,_=env.step(a)
+#                 all_r+=r
+#                 self.storeExp(s,a,r,s_)
+#                 if(self.memcnt>self.MEM_CAP):
+#                     self.learn()
+#                     if(done):
+#                         if(s_==self.nstate-1):
+#                             cnt_win+=1.0
+#                 s=s_
+#             if (i % 50 == 0):
+#                 print("period: ",i, ": ")
+#                 if (cnt_win / 50 > 0.4):
+#                      self.EPSILON += 0.01;
+#                 elif (cnt_win / 50 > 0.2):
+#                       self.EPSILON += 0.005;
+#                 elif (cnt_win / 50 > 0.1):
+#                       self.EPSILON += 0.003;
+#                 elif (cnt_win / 50 > 0.05):
+#                       self.EPSILON += 0.001;
+
+#                 print("current accuracy: %.2f %%" %(cnt_win/50.0*100))
+#                 win_rate.append(cnt_win / 50)
+#                 cnt_win=0
+#                 print("Global accuracy : %.2f %%" %(all_r / (i+1)*100))
+#         print("Global accuracy : ",all_r/numsteps*100,"%")
+#         plt.plot(win_rate)
+#         plt.show()
+
+# env = gym.make('FrozenLake-v0')
+# #env = gym.make('FrozenLake8x8-v0')
+# env = env.unwrapped
+# dqn=DQN(env.observation_space.n,env.action_space.n)
+# dqn.run(2000)
